f8b56c2cb50c316611c21eef135a28552087c117
Saranya Ravi <37731537+SharanPrav@users.noreply.github.com>
2023-09-23 16:52:55 +0200

    Initial Commit

---
+++ EssentialFeed/EssentialFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedApp.swift
+//  EssentialFeed
+//
+//  Created by Saranya Ravi on 23/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct EssentialFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
