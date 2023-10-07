97a92aab3090e2c34e83cefce9304fd5468892e0
Saranya Ravi <37731537+SharanPrav@users.noreply.github.com>
2023-10-07 23:18:49 +0200

    Moved HTTPClient and FeedItemsMapper into its own class

--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -7,16 +7,7 @@
 
 import Foundation
 
-public enum HTTPClientResult {
-    case success(Data, HTTPURLResponse)
-    case failure(Error)
-}
-
-public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
-}
-
-public final class RemoteFeedLoader {
+internal final class RemoteFeedLoader {
     private let url: URL
     private let client: HTTPClient
 
@@ -50,31 +41,6 @@ public final class RemoteFeedLoader {
             }
         }
     }
-}
-
-private class FeedItemsMapper {
-    private struct Root: Decodable {
-        let items: [Item]
-    }
-
-    public struct Item: Decodable {
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL
-        
-        var item: FeedItem {
-            return FeedItem(id: id, description: description, location: location, imageURL: image)
-        }
-    }
-    
-    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == 200 else {
-            throw RemoteFeedLoader.Error.invalidData
-        }
-        
-        return try JSONDecoder().decode(Root.self, from: data).items.map{ $0.item }
-    }
-}
+}
 
 
